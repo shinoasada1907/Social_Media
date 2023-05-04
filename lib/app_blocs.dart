61a63e1f492e9ee77ddbe298cf94ba1c280a18f2
shinoasada1907 <bonzed2001@gmail.com>
2023-05-04 23:18:58 +0700

    welcome bloc and create welcome UI

--- lib/app_blocs.dart
+++ lib/app_blocs.dart
@@ -6,7 +6,6 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc() : super(InitState()) {
     on<IncrementEvent>((event, emit) {
       emit(AppState(counter: state.counter + 1));
-      print(state.counter);
     });
 
     on<DecrementEvent>((event, emit) {
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,4 +1,10 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_events.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_states.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -10,9 +16,137 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Scaffold(),
+    return BlocBuilder<WelcomeBloc, WelcomeState>(
+      builder: (context, state) {
+        return Container(
+          color: Colors.white,
+          child: Scaffold(
+            body: Container(
+              margin: EdgeInsets.only(top: 34.h),
+              width: 375.w,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  PageView(
+                    onPageChanged: (index) {
+                      state.page = index;
+                      BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                      print(index);
+                    },
+                    children: [
+                      _page(
+                        1,
+                        context,
+                        'Next',
+                        'Fisrt See Learning',
+                        'Forget about a for of pager all knowledge in one learning!',
+                        'image path',
+                      ),
+                      _page(
+                        2,
+                        context,
+                        'Next',
+                        'Connect With Everyone',
+                        'Always keep in touch with your tutor & friend. let\'s get connected',
+                        'image path',
+                      ),
+                      _page(
+                        3,
+                        context,
+                        'Get Started',
+                        'Always Fascinated Learning',
+                        'Anywhere, anytime. The time is at your discretion so study whenever you want.',
+                        'image path',
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 100.h,
+                    child: DotsIndicator(
+                      position: state.page.toDouble(),
+                      dotsCount: 3,
+                      decorator: DotsDecorator(
+                        color: Colors.grey,
+                        activeColor: Colors.blue,
+                        size: const Size.square(8.0),
+                        activeSize: const Size(10.0, 8.0),
+                        activeShape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _page(int index, BuildContext context, String buttonName, String title,
+      String subtitle, String imagePath) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: const Text('Image one'),
+        ),
+        Container(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          width: 375.w,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            subtitle,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 14.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+          width: 325.w,
+          height: 50.w,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.w),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                blurRadius: 2,
+                spreadRadius: 1,
+                offset: const Offset(0, 1),
+              )
+            ],
+          ),
+          child: Center(
+            child: Text(
+              buttonName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
