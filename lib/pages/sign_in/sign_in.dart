0497f84fdc20067c19eebff89f232418e8a6a856
shinoasada1907 <bonzed2001@gmail.com>
2023-05-11 09:16:04 +0700

    Complete Sign In UI

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/pages/sign_in/widgets/sign_in_widget.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -19,8 +20,45 @@ class _SignInScreenState extends State<SignInScreen> {
           appBar: buildAppBar(),
           body: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 buildThirdPartyLogin(context),
+                Center(
+                  child: reusableText('Or use your email account login'),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 66.h),
+                  padding: EdgeInsets.only(
+                    left: 25.w,
+                    right: 25.w,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      reusableText('Email'),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      buildTextField(
+                        'Enter your email address',
+                        'email',
+                        'user',
+                      ),
+                      reusableText('Password'),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      buildTextField(
+                        'Enter your password',
+                        'password',
+                        'lock',
+                      ),
+                    ],
+                  ),
+                ),
+                forgotpassword(),
+                buildLoginAndRegButton('Log in', 'login'),
+                buildLoginAndRegButton('Register', 'register'),
               ],
             ),
           ),
--- lib/pages/sign_in/widgets/sign_in_widget.dart
+++ lib/pages/sign_in/widgets/sign_in_widget.dart
@@ -51,3 +51,144 @@ Widget _reusableIcons(String iconName) {
     ),
   );
 }
+
+Widget reusableText(String text) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 5.h),
+    child: Text(
+      text,
+      style: TextStyle(
+        color: Colors.grey.withOpacity(0.5),
+        fontWeight: FontWeight.normal,
+        fontSize: 14.sp,
+      ),
+    ),
+  );
+}
+
+Widget buildTextField(String hintText, String textStype, String iconName) {
+  return Container(
+    width: 325.w,
+    height: 50.h,
+    margin: EdgeInsets.only(bottom: 20.h),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(
+        Radius.circular(15.w),
+      ),
+      border: Border.all(color: Colors.black),
+    ),
+    child: Row(
+      children: [
+        Container(
+          width: 16.w,
+          height: 16.w,
+          margin: EdgeInsets.only(
+            left: 17.w,
+          ),
+          child: Image.asset('assets/icons/$iconName.png'),
+        ),
+        SizedBox(
+          width: 270.w,
+          height: 50.h,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              disabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              hintStyle: TextStyle(
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ),
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Avenir',
+              fontWeight: FontWeight.normal,
+              fontSize: 14.sp,
+            ),
+            autocorrect: false,
+            obscureText: textStype == 'password' ? true : false,
+            keyboardType: TextInputType.emailAddress,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget forgotpassword() {
+  return Container(
+    margin: EdgeInsets.only(left: 25.w),
+    width: 260.w,
+    height: 44.h,
+    child: GestureDetector(
+      onTap: () {},
+      child: Text(
+        'Forgot password',
+        style: TextStyle(
+          color: Colors.black,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.blue,
+          fontSize: 12.sp,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildLoginAndRegButton(String buttonName, String buttonType) {
+  return GestureDetector(
+    onTap: () {},
+    child: Container(
+      margin: EdgeInsets.only(
+        left: 25.w,
+        right: 25.w,
+        top: buttonType == 'login' ? 40.h : 20.h,
+      ),
+      width: 325.w,
+      height: 50.h,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15.w),
+        ),
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 1,
+            blurRadius: 2,
+            color: Colors.grey.withOpacity(0.1),
+            offset: const Offset(0, 1),
+          )
+        ],
+      ),
+      child: Center(
+        child: Text(
+          buttonName,
+          style: TextStyle(
+            fontSize: 16.sp,
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
+}
